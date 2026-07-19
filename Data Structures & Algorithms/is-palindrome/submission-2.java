e3ffa908612597df6825f89f591a007fdb04963a
Miguel Córdoba <80487349+miguel-cordoba@users.noreply.github.com>
2026-07-19 13:49:54 +0200

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9^]", "");
+        s = s.toLowerCase();
+
+        int j = s.length() - 1;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            } 
+            j--;
+        }
+        return true;
+    }
+}
